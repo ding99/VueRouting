20b9079f34f4c59329c1846c4918555fc76965a2
Wei Ding <ding_web@yahoo.co.jp>
2021-02-18 22:46:30 -0500

    v-html

---
+++ exam01/src/components/Vhtml.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h2>v-html</h2>
+		<p>{{ message }}</p>
+        <div v-html = "message"></div>
+	</div>
+</template>
+
+<script>
+	export default{
+		data: function() {
+			return {
+				message: '<h3>Hello, v-html directive!</h3>'
+			}
+		}
+	}
+</script>
