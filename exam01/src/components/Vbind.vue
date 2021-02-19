c73682fc1b5800df499ac01f394491c0947739b5
Wei Ding <ding_web@yahoo.co.jp>
2021-02-18 23:59:29 -0500

    v bind

---
+++ exam01/src/components/Vbind.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<h2>V-bind</h2>
+		<label for = "r1">change color</label>
+		<input type = "checkbox" v-model = "use" id = "r1">
+		<br>
+		<div v-bind:class = "{'class1': use }">v-bind:class directive</div>
+	</div>
+</template>
+
+<script>
+	//export default{
+	var	data = {use: false}
+	//}
+</script>
+
+<style>
+	.class1 {
+		background: #444;
+		color: rgba(248, 244, 3, 0.89);
+	}
+</style>
--- exam01/src/route-config.js
+++ exam01/src/route-config.js
@@ -1,9 +1,11 @@
 import Home from './components/Home.vue'
 import About from './components/About.vue'
 import Vhtml from './components/Vhtml.vue'
+import Vbind from './components/Vbind.vue'
 
 export default {
 	'home': { component: Home },
 	'about': { component: About },
-	'vhtml': { component: Vhtml }
+	'vhtml': { component: Vhtml },
+	'vbind': { component: Vbind }
 }
